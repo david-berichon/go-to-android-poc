507677d1d1b8ab24f1d188f5c59c31fdae0f4cc6
david-berichon <davidberich@gmail.com>
2016-11-05 09:37:08 +0100

    Add main pkg

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,23 @@
 package hello
 
+//#include <stdlib.h>
+import "C"
+import (
+	"fmt"
+	"time"
+)
+
 // export Reverse
-func Reverse(s string) string {
-	r := []rune(s)
+func Reverse(s *C.char) *C.char {
+	C.srand(C.uint(time.Now().Nanosecond()))
+	fmt.Println(C.rand())
+	r := []rune(C.GoString(s))
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
-	return string(r)
+	return C.CString(string(r))
+}
+
+func GoReverse(s string) string {
+	return C.GoString(Reverse(C.CString(s)))
 }
---
+++ main/main.go
@@ -0,0 +1,12 @@
+// main
+package main
+
+import (
+	"fmt"
+
+	"github.com/david-berichon/go-to-android-poc/hello"
+)
+
+func main() {
+	fmt.Println(hello.GoReverse("Hello world !"))
+}
